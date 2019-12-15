707a6c11006796dd8ec9c8942c9315019eedcd22
MARCIO MACHADO PONTES <43865281+marcinhojazz@users.noreply.github.com>
2019-12-14 22:08:11 -0300

    Add files via upload

---
+++ globalCliente.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(global.MinhaApp.saudacao())
+
+MinhaApp.nome = 'Eita!'
+console.log(MinhaApp.nome)
---
+++ usandoModulosTerceiro.js
@@ -0,0 +1,2 @@
+const _ = require('loadash')
+setInterval(() => console.log(_.random(1, 500)), 2000)
